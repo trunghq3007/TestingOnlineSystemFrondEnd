817f494ca7dc650f08124f6eb32d3a60e83d5e3f
vannguyen21697 <nguyenvan21697@gmail.com>
2019-05-13 11:24:14 +0700

    new frontend version

---
+++ src/app/view-list-question/view-list-question.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-list-question',
+  templateUrl: './view-list-question.component.html',
+  styleUrls: ['./view-list-question.component.scss']
+})
+export class ViewListQuestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
