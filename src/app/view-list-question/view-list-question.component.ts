9a07c45f408d1704ca15b508d0b48efe6b46934a
= <=>
2019-05-22 11:20:26 +0700

    nhvan

--- src/app/view-list-question/view-list-question.component.ts
+++ src/app/view-list-question/view-list-question.component.ts
@@ -5,45 +5,61 @@ import { Router } from '@angular/router';
 import { MatPaginator, MatTableDataSource, MatSort } from '@angular/material';
 import { SelectionModel } from '@angular/cdk/collections';
 import { ToastrService } from 'ngx-toastr';
+import { FormGroup, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-view-list-question',
   templateUrl: './view-list-question.component.html',
   styleUrls: ['./view-list-question.component.scss']
 })
 export class ViewListQuestionComponent implements OnInit {
+  const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
   searchString: string;
   questionId = '';
   Question: Question[] = [];
+  tagsFormApi: [];
+  categoriesFormApi: [];
   constructor(private http: HttpClient, private router: Router, private toastr: ToastrService) { }
   displayedColumn: string[] = ['select', 'Category', 'CreatedBy', 'CreatedDate', 'Level', 'Content', 'Tag', 'Action'];
   dataSource = new MatTableDataSource<Question>(this.Question);
-
+  formFillter: FormGroup = new FormBuilder().group({
+    CreatedBy: '',
+    StartDate: '',
+    ToDate: '',
+    Level: '',
+    TagsId: '',
+    CategoryId: '',
+    Type: ''
+  });
   selection = new SelectionModel<Question>(true, []);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   ngOnInit() {
+    this.getApiCategories();
+    this.getApiTags();
     this.http.get<string>('http://localhost:65170/api/question/').subscribe(value => {
       let source = JSON.parse(value);
       let tagNames = '';
       for (let index = 0; index < source.length; index++) {
         let element = source[index];
-
         let tag = element.Tags;
         if (tag && tag.length > 0) {
           for (let i = 0; i < tag.length; i++) {
             tagNames += tag[i].Name + ', ';
           }
         }
+        if(element.Level ==1) element.LevelString="Easy";
+        if(element.Level ==2) element.LevelString="Medium";
+        if(element.Level ==3) element.LevelString="Difficult";
         element.TagNames = tagNames;
       };
-      console.log(source);
       this.dataSource.data = source;
-      console.log(this.dataSource.data);
-
       this.dataSource.paginator = this.paginator, this.dataSource.sort = this.sort;
     });
   }
+
   // deleteQuestion(questionId: string) {
   //   this.http.delete('http://localhost:65170/api/question/' + questionId).subscribe(() => {
   //     this.dataSource.data = this.dataSource.data.filter(question => question.Id !== questionId);
@@ -91,10 +107,8 @@ export class ViewListQuestionComponent implements OnInit {
       PageSize: 10,
       searchString: this.searchString
     }
-    const httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-    };
-    this.http.post<string>('http://localhost:65170/api/question?action=search', JSON.stringify(searchObject), httpOptions).subscribe(value => {
+
+    this.http.post<string>('http://localhost:65170/api/question?action=search', JSON.stringify(searchObject), this.httpOptions).subscribe(value => {
       this.Question = JSON.parse(value);
       this.dataSource.paginator = this.paginator, this.dataSource.sort = this.sort;
     })
@@ -121,4 +135,38 @@ export class ViewListQuestionComponent implements OnInit {
     }
     return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.Id + 1}`;
   }
+  getApiTags() {
+    this.http.get<string>('http://localhost:65170/api/tag/').subscribe(value => {
+      this.tagsFormApi = JSON.parse(value);
+    });
+  }
+  getApiCategories() {
+    this.http.get<string>('http://localhost:65170/api/category/').subscribe(value => {
+      this.categoriesFormApi = JSON.parse(value);
+    });
+  }
+  fillterClick() {
+    
+
+    console.log(this.formFillter.value);
+    this.http.post<string>('http://localhost:65170/api/question?action=fillter', JSON.stringify(this.formFillter.value), this.httpOptions).subscribe(value => {
+      debugger;
+    let source = JSON.parse(value);
+      let tagNames = '';
+      for (let index = 0; index < source.length; index++) {
+        let element = source[index];
+  
+        let tag = element.Tags;
+        if (tag && tag.length > 0) {
+          for (let i = 0; i < tag.length; i++) {
+            tagNames += tag[i].Name + ', ';
+          }
+        }
+        element.TagNames = tagNames;
+      };
+      this.dataSource.data = source;
+      this.dataSource.paginator = this.paginator, this.dataSource.sort = this.sort;
+    });
+  }
+
 }
