144c63724da82b02f853a7df5874b33c3736d2d0
vannguyen21697 <nguyenvan21697@gmail.com>
2019-05-29 14:46:39 +0700

    vann
    Signed-off-by: vannguyen21697 <nguyenvan21697@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ViewListQuestionComponent } from './view-list-question/view-list-question.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CreateQuestionComponent } from './create-question/create-question.component';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import * as ClassicEditor from '@ckeditor/ckeditor5-angular';
 
 @NgModule({
   declarations: [
@@ -26,9 +28,22 @@ import { CreateQuestionComponent } from './create-question/create-question.compo
   imports: [
     BrowserModule,
     HttpClientModule,
+    CKEditorModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  public Editor = ClassicEditor;
+
+  public onReady(editor) {
+    editor.ui.getEditableElement().parentElement.insertBefore(
+      editor.ui.view.toolbar.element,
+      editor.ui.getEditableElement()
+    );
+  }
+
+}
+
+
