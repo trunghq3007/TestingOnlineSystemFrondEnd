8e601d5ccfecadc92d227affc5efc2dd8f480a48
nhat <nguyennhatt098@gmail.com>
2019-06-25 13:56:01 +0700

    update exception

--- src/app/errorpage/errorpage.component.ts
+++ src/app/errorpage/errorpage.component.ts
@@ -8,39 +8,47 @@ import { Router } from '@angular/router';
   styleUrls: ['./errorpage.component.scss']
 })
 export class ErrorpageComponent implements OnInit {
-errors:string;
-status;
-message;
-description;
-checked=true;
-  constructor(private myservice:MyserviceService,private router:Router) {
+  errors: string;
+  status;
+  message;
+  description;
+  checked = true;
+  constructor(private myservice: MyserviceService, private router: Router) {
     this.router.events.subscribe((event) => {
       this.myservice.changeMessage('2');
-   });
-   }
+    });
+  }
 
   ngOnInit() {
-    
-    
+
+
     console.log(this.checked)
     this.myservice.currentError.subscribe(message => this.errors = message);
-    var ListErrors=this.errors.split(',');
+    var ListErrors = this.errors.split(',');
     console.log(ListErrors);
-    this.status=ListErrors[0];
-  
+    this.status = ListErrors[0];
+
     // console.log('sdsf'+this.message);
-    if(this.status==401){
-      this.message=ListErrors[1];
-      this.description=" Contact admin to access this feature";
-    }else if(this.status==500){
-      this.message=ListErrors[1];
-      this.description=" Error! An error occurred. Please try again later";
+    if (this.status == 401) {
+      this.message = ListErrors[1];
+      this.description = " Contact admin to access this feature";
+    } else if (this.status == 500) {
+      this.message = ListErrors[1];
+      this.description = " Error! An error occurred. Please try again later";
+    } else if (this.status == 400) {
+      this.message = ListErrors[1];
+      this.description = " Something needs your attention";
     }
-    
-    if(this.status!=""){
-      this.checked=false
-    }else{
-      this.checked=true
+
+    else {
+      this.message = ListErrors[1];
+      this.description = " Error! An error occurred. Please try again later";
+    }
+
+    if (this.status != "") {
+      this.checked = false
+    } else {
+      this.checked = true
 
     }
   }
